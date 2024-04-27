911c1853a30ccacd47bdd32d7070dbbcab348c90
Gaurav Sharma <gauravsharmatheofficial@gmail.com>
2024-04-27 16:45:59 +0530

    Todo app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="component">
+        <div className="bg-red-600">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
